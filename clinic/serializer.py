2db99fce6527bbed46447ea41e5744240d3dacf7
Mohamed Hamza <mohammedalihamza77@gmail.com>
2021-08-23 21:46:46 +0200

    chore: update urls

--- clinic/serializer.py
+++ clinic/serializer.py
@@ -3,19 +3,23 @@ from rest_framework import serializers
 from user.models import User
 
 class ClinicDateSerializers(serializers.ModelSerializer):
-       #clinic = serializers.PrimaryKeyRelatedField(many=True, read_only=True)       
        class Meta:
               model = ClinicDate
               fields = ['day','start_time', 'end_time']
 
+
 class ClinicSerializers(serializers.ModelSerializer):
-       #doctor = serializers.StringRelatedField()
+       doctor = serializers.StringRelatedField(required=False)
        date = ClinicDateSerializers(many=True)
-       #date = serializers.PrimaryKeyRelatedField(many=True, read_only=True) 
        class Meta:
               model = Clinic
               fields = ['name', 'price', 'doctor', 'date']
 
        def create(self, validated_data):
+              dates_data = validated_data.pop('date')
               user =  self.context['request'].user
-              return Clinic.objects.create(**validated_data, doctor=user)       
+              clinic = Clinic.objects.create(doctor=user, **validated_data)
+              for date_data in dates_data:
+                     dt = ClinicDate.objects.create(**date_data)
+                     clinic.date.add(dt)
+              return clinic
--- clinic/urls.py
+++ clinic/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-       path('clinic/', views.ClinicListView.as_view(), name='clinic-list'),
-       path('clinic/add/', views.ClinicCreateView.as_view(), name='clinic-create'),
-       path('clinic/update/', views.ClinicUpdateDeleteView.as_view(), name='clinic-update'),
+       path('', views.ClinicListView.as_view(), name='clinic-list'),
+       path('add/', views.ClinicCreateView.as_view(), name='clinic-create'),
+       path('update-delete/', views.ClinicUpdateDeleteView.as_view(), name='clinic-update'),
        #path('', views.ClinicListView.as_view(), name='clinic-list'),
 ]
