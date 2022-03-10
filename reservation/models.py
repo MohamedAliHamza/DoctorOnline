2855aac74e80a154c9cf030c8c70b004244704c6
MohamedAliHamza <mohammedalihamza77@gmail.com>
2022-03-10 03:38:04 +0200

    Update Reservation model

--- reservation/models.py
+++ reservation/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-from user.models import User
-from utility.models import DateModel
-from specialty.models import Clinic
 
-class Reservation(DateModel):
-       patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_reservation')
-       doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doctor_reservation')
-       clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE, related_name='clinic_reservation')
+from .constants import RESERVATION_STATUS, RESERVATION_STATUS_CHOICES
 
-       def __str__(self):
-              return str(self.id)
+from doctor.constants import Day_CHOICES
+from utility.models import TimeStamp
+from patient.models import Patient
+from doctor.models import Doctor
+
+
+class Reservation(TimeStamp):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='+')
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='+')
+    status = models.IntegerField(choices=RESERVATION_STATUS_CHOICES, default=RESERVATION_STATUS.PENDING)
+    day = models.IntegerField(choices=Day_CHOICES)
+    time = models.TimeField()
+    
+    def __str__(self):
+       return str(self.id)
