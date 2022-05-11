f058e853ccb7b8f61ce495b4aefb665fe88bb50f
MohamedAliHamza <mohammedalihamza77@gmail.com>
2022-05-11 17:08:46 +0200

    Update patient model

---
+++ patient/constants.py
@@ -0,0 +1,11 @@
+
+
+class Gender:
+    MALE = 0
+    FEMALE = 1
+
+
+GENDER_CHOICES = [
+    (Gender.MALE, 'Male'),
+    (Gender.FEMALE, 'Female'),
+]
--- patient/models.py
+++ patient/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+from .constants import GENDER_CHOICES
+
 from common.models import TimeStamp
 from user.models import User
 
 
 class Patient(TimeStamp):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient')
     full_name = models.CharField(max_length=120)
-    avatar = models.ImageField(upload_to='media/patients/', default='media/patients/default.jpg')
+    avatar = models.ImageField(upload_to='patients/', default='patients/default.jpg')
+    age = models.IntegerField()
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    length = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.full_name
